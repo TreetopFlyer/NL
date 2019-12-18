c6b0d4bef3d9123ced20bb75a1a411d2d090c2ad
TreetopFlyer <seth111@gmail.com>
2019-12-18 09:10:43 -0500

    wiring setup

--- network.ts
+++ network.ts
@@ -1,7 +1,13 @@
 import {Connection, Neuron, Layer} from './nl';
 
-var l1:Layer;
+var l1:Layer, l2:Layer;
 
 l1 = new Layer(5, 5);
+l2 = new Layer(5, 5);
+
 l1.WireLateral(3, 0.5);
+l2.WireLateral(3, 0.5);
+l2.WireReceptive(3, 0.5, l1);
+
+l2.IterateRadial(0.2, -1.2, 3, (inNeuron, inX, inY, inDistance)=>console.log(inNeuron, inX, inY, inDistance));
 console.log(l1);
